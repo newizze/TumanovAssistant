ea43df15e6603d523c025f2a3fe2a2005467f171
Newizze <info@newizze.com>
2025-10-15 13:26:00 +0100

    Fixes

--- app/Tools/AddRowToSheetsToolHandler.php
+++ app/Tools/AddRowToSheetsToolHandler.php
@@ -58,22 +58,38 @@ class AddRowToSheetsToolHandler
             $executorInfo = collect($executors)->firstWhere('short_code', $arguments['executor'] ?? '');
 
             // Подготавливаем данные для строки согласно структуре таблицы
+            // Структура: A-AD (30 колонок)
             $rowData = [
-                $taskId, // ID
-                date('d.m.Y H:i:s'), // Дата создания
-                $arguments['sender_name'] ?? '', // Отправитель ФИО
-                $arguments['executor'] ?? '', // Исполнитель
-                $arguments['task_type'] ?? '', // Тип задачи
-                $arguments['task_title'], // Краткое название
-                $arguments['task_description'], // Подробное описание
-                $arguments['expected_result'] ?? '', // Ожидаемый конечный результат
-                $arguments['priority'], // Приоритет
-                $arguments['file_link_1'] ?? '', // Ссылка на файл отправителя
-                $arguments['file_link_2'] ?? '', // Ссылка на файл отправителя2
-                $arguments['file_link_3'] ?? '', // Ссылка на файл отправителя3
-                '', // План исполнителя (не заполняем)
-                'Неразобранная', // Статус
-                $arguments['requires_verification'] ?? 'Нет', // Требуется ли проверка
+                $taskId, // A: ID
+                date('d.m.Y H:i:s'), // B: Дата создания
+                $arguments['sender_name'] ?? '', // C: Отправитель ФИО
+                $arguments['executor'] ?? '', // D: Исполнитель
+                $arguments['task_type'] ?? '', // E: Тип задачи1
+                $arguments['task_title'], // F: Краткое название
+                $arguments['task_description'], // G: Подробное описание
+                $arguments['expected_result'] ?? '', // H: Ожидаемый конечный результат
+                $arguments['priority'], // I: Приоритет
+                $arguments['file_link_1'] ?? '', // J: Ссылка на файл отправителя
+                $arguments['file_link_2'] ?? '', // K: Ссылка на файл отправителя2
+                $arguments['file_link_3'] ?? '', // L: Ссылка на файл отправителя3
+                '', // M: План исполнителя
+                'Неразобранная', // N: Статус
+                '', // O: Дата факт готово
+                '', // P: Приложение от исполнителя
+                '', // Q: Комментарий исполнителя
+                '', // R: Дата первого выхода из Неразобранная
+                '', // S: Почта сотрудника
+                '', // T: Приоритет.
+                '', // U: Тг сотрудника
+                '', // V: Ссылка на задачу
+                '', // W: Тех
+                '', // X: Тип задачи
+                '', // Y: Диагноз (корень проблемы)
+                '', // Z: Что сделано, чтобы не повторилось
+                '', // AA: Чек отправки готового/отмененного
+                '', // AB: Почта исполнителя
+                '', // AC: Лог
+                $arguments['requires_verification'] ?? 'Нет', // AD: Требуется ли проверка
             ];
 
             // Добавляем строку в таблицу
